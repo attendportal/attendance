18a399dbe23094c9e6f38c335930f3728c5a4443
prismco <prizamkoirala@gmail.com>
2022-07-27 08:25:44 +0545

    New Deployment

---
+++ precache-manifest.df0b351a20e1c122d678b16b1d5835b3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf0e8fbe3c346706183",
+    "url": "/attendance/css/chunk-vendors.638cd1af.css"
+  },
+  {
+    "revision": "023656e5654a03c45a10",
+    "url": "/attendance/css/home.32442c26.css"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/attendance/fonts/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/attendance/fonts/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/attendance/fonts/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/attendance/fonts/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/attendance/fonts/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/attendance/fonts/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "6980b57dc4422f327231aa977f4786a3",
+    "url": "/attendance/img/logo.6980b57d.svg"
+  },
+  {
+    "revision": "3f02633a1cd17bcbc06d17446644bcaf",
+    "url": "/attendance/index.html"
+  },
+  {
+    "revision": "ba1b0c6a377d484f7de5",
+    "url": "/attendance/js/about.35580eed.js"
+  },
+  {
+    "revision": "9cfef6c5ce0353a7ab5c",
+    "url": "/attendance/js/about~home.60ec57e9.js"
+  },
+  {
+    "revision": "de85bd150728dfccbb62",
+    "url": "/attendance/js/app.e1ee86eb.js"
+  },
+  {
+    "revision": "831e0d679ea3ee140947",
+    "url": "/attendance/js/chunk-2d0b1f6a.27140ac2.js"
+  },
+  {
+    "revision": "dbf0e8fbe3c346706183",
+    "url": "/attendance/js/chunk-vendors.1d0ad112.js"
+  },
+  {
+    "revision": "023656e5654a03c45a10",
+    "url": "/attendance/js/home.f4a13888.js"
+  },
+  {
+    "revision": "9d9833cbd76b80a2da281e15020e4734",
+    "url": "/attendance/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/attendance/robots.txt"
+  }
+]);
