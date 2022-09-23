bab24a47f8742cfdab41da39d2f2847e53a33234
prismco <prizamkoirala@gmail.com>
2022-09-23 11:34:21 +0545

    New Deployment

---
+++ precache-manifest.a01ea2cf076f407a71af187562440426.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e968d05dc650388b0b51",
+    "url": "/attendance/css/chunk-vendors.24ac48f5.css"
+  },
+  {
+    "revision": "27e9a05bab67d5f33511",
+    "url": "/attendance/css/home.6e0240bf.css"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/attendance/fonts/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/attendance/fonts/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/attendance/fonts/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/attendance/fonts/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/attendance/fonts/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/attendance/fonts/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "939d76e3c859dd87a9401ab62c52e314",
+    "url": "/attendance/img/admin.939d76e3.svg"
+  },
+  {
+    "revision": "875719acb4ee4d8fdfb8c4798e17ea21",
+    "url": "/attendance/img/alert.875719ac.svg"
+  },
+  {
+    "revision": "5188382db5911a80872abbe3258c05e7",
+    "url": "/attendance/img/att-list.5188382d.svg"
+  },
+  {
+    "revision": "53fe35a6811a8ab9a49e9fad007e4589",
+    "url": "/attendance/img/attendance.53fe35a6.svg"
+  },
+  {
+    "revision": "035146453639b9276b97f5cf55f33afb",
+    "url": "/attendance/img/company-logo.03514645.jpg"
+  },
+  {
+    "revision": "84a8c75900b444bb06b6de23504904d4",
+    "url": "/attendance/img/group.84a8c759.svg"
+  },
+  {
+    "revision": "6980b57dc4422f327231aa977f4786a3",
+    "url": "/attendance/img/logo.6980b57d.svg"
+  },
+  {
+    "revision": "9ce2f114e05f21d7051f52055d7f54c0",
+    "url": "/attendance/img/material-rate.9ce2f114.svg"
+  },
+  {
+    "revision": "54b245ce52f99eaaeb8ec77da9b242a1",
+    "url": "/attendance/img/material-type.54b245ce.svg"
+  },
+  {
+    "revision": "c8005ce099c1c2ab5759c0bcef197c24",
+    "url": "/attendance/img/material.c8005ce0.svg"
+  },
+  {
+    "revision": "8b3a9af8d8991a6f77975197c3989687",
+    "url": "/attendance/img/rate.8b3a9af8.svg"
+  },
+  {
+    "revision": "e50813fb3abb92f22f1dc8f1d45a2374",
+    "url": "/attendance/img/report.e50813fb.svg"
+  },
+  {
+    "revision": "46c864323e9ee49e1258024905befb28",
+    "url": "/attendance/img/resource.46c86432.svg"
+  },
+  {
+    "revision": "256a6b0eff5f6dda027b88dba2911a67",
+    "url": "/attendance/img/site.256a6b0e.svg"
+  },
+  {
+    "revision": "0c5d4e796001cf39abb98eb1510e5316",
+    "url": "/attendance/img/supplier.0c5d4e79.svg"
+  },
+  {
+    "revision": "773946da5dbd1f985061d5a8d38c7f0b",
+    "url": "/attendance/img/user.773946da.svg"
+  },
+  {
+    "revision": "ac3ef4736ffd3ded93b409b07b85a7c4",
+    "url": "/attendance/index.html"
+  },
+  {
+    "revision": "7ec24cbce2528a3325cc",
+    "url": "/attendance/js/about.131d35d6.js"
+  },
+  {
+    "revision": "4fb10babcb8317efaa0a",
+    "url": "/attendance/js/about~home.abbda619.js"
+  },
+  {
+    "revision": "f1f7a8bdc0f84935b2e2",
+    "url": "/attendance/js/app.a3ea2aa1.js"
+  },
+  {
+    "revision": "831e0d679ea3ee140947",
+    "url": "/attendance/js/chunk-2d0b1f6a.27140ac2.js"
+  },
+  {
+    "revision": "e968d05dc650388b0b51",
+    "url": "/attendance/js/chunk-vendors.ffeb4515.js"
+  },
+  {
+    "revision": "27e9a05bab67d5f33511",
+    "url": "/attendance/js/home.5cf3c9f2.js"
+  },
+  {
+    "revision": "9d9833cbd76b80a2da281e15020e4734",
+    "url": "/attendance/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/attendance/robots.txt"
+  }
+]);
